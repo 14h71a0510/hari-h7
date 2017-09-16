a6ec44731ce85fe24f5fbfa447935debffa730aa
14h71a0510 <32010223+14h71a0510@users.noreply.github.com>
2017-09-16 15:24:01 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+
+public class Main {
+	public static void main(String[] args) {
+
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+
+    Student syam = new Student("hari", "krishna", "1996");
+    System.out.println(syam);
+    
+    Student tharun = new Student("gopi mankinta", "gopi", "1997");
+    System.out.println(tharun);
+    
+    System.out.println("hari compared to gopi: " + hari.compareTo(gopi));
+    System.out.println("gopi compared to hari: " + gopi.compareTo(hari));
+ 
+	}
+
+}
